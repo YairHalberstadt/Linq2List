b0aeb66e035dd86eabb37fe03d56972c3d46481e
yair halberstadt <yairhalberstadt@hotmail.co.uk>
2018-11-07 16:03:38 +0000

    Iterator now implements IList, for improved performance with linq

--- IReadOnlyListLinq/IReadOnlyListLinq.Iterator.cs
+++ IReadOnlyListLinq/IReadOnlyListLinq.Iterator.cs
@@ -1,47 +1,119 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace IReadOnlyListLinq
 {
-    public static partial class IReadOnlyListLinq
-    {
-        private abstract class Iterator<T> : IReadOnlyList<T>, IEnumerator<T>
-        {
-            protected bool _enumeratorTaken;
-            protected T _current;
-            protected int _index = -1;
+	public static partial class IReadOnlyListLinq
+	{
+		private abstract class Iterator<T> : IReadOnlyList<T>, IEnumerator<T>, IList<T>
+		{
+			protected T current;
+			private bool _enumeratorTaken;
+			protected int index = -1;
 
-            public abstract T this[int index] { get; }
-            public abstract int Count { get; }
-            public T Current => _current;
-            object IEnumerator.Current => Current;
+			public T Current => current;
+			object IEnumerator.Current => Current;
 
-            public abstract Iterator<T> Clone();
-            public IEnumerator<T> GetEnumerator()
-            {
-                var enumerator = _enumeratorTaken ? Clone() : this;
-                enumerator._enumeratorTaken = true;
-                return enumerator;
-            }
+			public abstract bool MoveNext();
 
-            public abstract bool MoveNext();
+			public virtual void Dispose()
+			{
+				current = default;
+				index = -1;
+			}
 
-            public virtual void Dispose()
-            {
-                _current = default;
-                _index = -1;
-            }
+			public void Reset()
+			{
+				current = default;
+				index = -1;
+			}
 
-            public void Reset()
-            {
-                _current = default;
-                _index = -1;
-            }
+			public abstract T this[int index] { get; }
 
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-        }
-    }
+			public abstract int Count { get; }
+
+			public IEnumerator<T> GetEnumerator()
+			{
+				var enumerator = _enumeratorTaken ? Clone() : this;
+				enumerator._enumeratorTaken = true;
+				return enumerator;
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				return GetEnumerator();
+			}
+
+			public abstract Iterator<T> Clone();
+
+			#region IList Implementation
+
+			int IList<T>.IndexOf(T item)
+			{
+				for (int i = 0; i < Count; i++)
+					if (this[i].Equals(item))
+						return i;
+
+				return -1;
+			}
+
+			void IList<T>.Insert(int index, T item)
+			{
+				throw new NotSupportedException();
+			}
+
+			void IList<T>.RemoveAt(int index)
+			{
+				throw new NotSupportedException();
+			}
+
+			void ICollection<T>.Add(T item)
+			{
+				throw new NotSupportedException();
+			}
+
+			void ICollection<T>.Clear()
+			{
+				throw new NotSupportedException();
+			}
+
+			bool ICollection<T>.Contains(T item)
+			{
+				for (int i = 0; i < Count; i++)
+					if (this[i].Equals(item))
+						return true;
+
+				return false;
+			}
+
+			void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+			{
+				if (array is null)
+					throw new ArgumentNullException(nameof(array));
+				if (arrayIndex < 0)
+					throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+				if (array.Length - arrayIndex < Count)
+					throw new ArgumentException(
+						$"The number of elements in the source collection is greater than the available space from {nameof(arrayIndex)} to the end of the destination array.");
+				for (int i = 0; i < Count; i++, arrayIndex++)
+					array[arrayIndex] = this[i];
+			}
+
+			bool ICollection<T>.Remove(T item)
+			{
+				throw new NotSupportedException();
+			}
+
+			bool ICollection<T>.IsReadOnly => true;
+
+			T IList<T>.this[int index]
+			{
+				get => this[index];
+				set => throw new NotSupportedException();
+			}
+
+			#endregion
+		}
+	}
 }
