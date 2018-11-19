d6fa24a525e41d900b79a9b8c05e7c01d5f7a133
yair halberstadt <yairhalberstadt@gmail.com>
2018-11-19 21:44:15 +0000

    Added Cast

---
+++ IReadOnlyListLinq.Tests.Unit/CastTests.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace IReadOnlyListLinq.Tests.Unit
+{
+    public class CastTests : IReadOnlyListLinqTests
+    {
+        #region IReadOnlyList CastTests
+        [Fact]
+        public void CastIntToLongThrows()
+        {
+            var q = from x in new[] { 9999, 0, 888, -1, 66, -777, 1, 2, -12345 }
+                    select x;
+
+            var rst = q.Cast<int, long>();
+
+            Assert.Throws<InvalidCastException>(() => { foreach (var t in rst) ; });
+        }
+
+        [Fact]
+        public void CastByteToUShortThrows()
+        {
+            var q = from x in new byte[] { 0, 255, 127, 128, 1, 33, 99 }
+                    select x;
+
+            var rst = q.Cast<byte, ushort>();
+            Assert.Throws<InvalidCastException>(() => { foreach (var t in rst) ; });
+        }
+
+        [Fact]
+        public void EmptySource()
+        {
+            object[] source = { };
+            Assert.Empty(source.Cast<object, int>());
+
+        }
+
+        [Fact]
+        public void NullableIntFromAppropriateObjects()
+        {
+            int? i = 10;
+            object[] source = { -4, 1, 2, 3, 9, i };
+            int?[] expected = { -4, 1, 2, 3, 9, i };
+
+            Assert.Equal(expected, source.Cast<object, int?>());
+        }
+
+        [Fact]
+        public void LongFromNullableIntInObjectsThrows()
+        {
+            int? i = 10;
+            object[] source = { -4, 1, 2, 3, 9, i };
+
+            var cast = source.Cast<object, long>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void LongFromNullableIntInObjectsIncludingNullThrows()
+        {
+            int? i = 10;
+            object[] source = { -4, 1, 2, 3, 9, null, i };
+
+            var cast = source.Cast<object, long?>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void NullableIntFromAppropriateObjectsIncludingNull()
+        {
+            int? i = 10;
+            object[] source = { -4, 1, 2, 3, 9, null, i };
+            int?[] expected = { -4, 1, 2, 3, 9, null, i };
+
+            Assert.Equal(expected, source.Cast<object, int?>());
+        }
+
+        [Fact]
+        public void ThrowOnUncastableItem()
+        {
+            object[] source = { -4, 1, 2, 3, 9, "45", 11 };
+            int[] expectedBeginning = { -4, 1, 2, 3, 9 };
+
+            var cast = source.Cast<object, int>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+            Assert.Equal(expectedBeginning, cast.Take(5));
+            Assert.Throws<InvalidCastException>(() => cast.ElementAt(5));
+            Assert.Throws<InvalidCastException>(() => cast[5]);
+            Assert.Equal(11, cast[6]);
+        }
+
+        [Fact]
+        public void ThrowCastingIntToDouble()
+        {
+            int[] source = new int[] { -4, 1, 2, 9 };
+
+            var cast = source.Cast<int, double>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        private static void TestCastThrow<T>(object o)
+        {
+            byte? i = 10;
+            object[] source = { -1, 0, o, i };
+
+            var cast = source.Cast<object, T>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void ThrowOnHeterogenousSource()
+        {
+            TestCastThrow<long?>(null);
+            TestCastThrow<long>(9L);
+        }
+
+        [Fact]
+        public void CastToString()
+        {
+            object[] source = { "Test1", "4.5", null, "Test2" };
+            string[] expected = { "Test1", "4.5", null, "Test2" };
+
+            Assert.Equal(expected, source.Cast<object, string>());
+        }
+
+        [Fact]
+        public void ArrayConversionThrows()
+        {
+            Assert.Throws<InvalidCastException>(() => new[] { -4 }.Cast<int, long>().ToList());
+        }
+
+        [Fact]
+        public void FirstElementInvalidForCast()
+        {
+            object[] source = { "Test", 3, 5, 10 };
+
+            var cast = source.Cast<object, int>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void LastElementInvalidForCast()
+        {
+            object[] source = { -5, 9, 0, 5, 9, "Test" };
+
+            var cast = source.Cast<object, int>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void NullableIntFromNullsAndInts()
+        {
+            object[] source = { 3, null, 5, -4, 0, null, 9 };
+            int?[] expected = { 3, null, 5, -4, 0, null, 9 };
+
+            Assert.Equal(expected, source.Cast<object, int?>());
+        }
+
+        [Fact]
+        public void ThrowCastingIntToLong()
+        {
+            int[] source = new int[] { -4, 1, 2, 3, 9 };
+
+            var cast = source.Cast<int, long>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void ThrowCastingIntToNullableLong()
+        {
+            int[] source = new int[] { -4, 1, 2, 3, 9 };
+
+            var cast = source.Cast<int, long?>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void ThrowCastingNullableIntToLong()
+        {
+            int?[] source = new int?[] { -4, 1, 2, 3, 9 };
+
+            var cast = source.Cast<int?, long>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void ThrowCastingNullableIntToNullableLong()
+        {
+            int?[] source = new int?[] { -4, 1, 2, 3, 9, null };
+
+            var cast = source.Cast<int?, long?>();
+            Assert.Throws<InvalidCastException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void CastingNullToNonnullableIsNullReferenceException()
+        {
+            int?[] source = new int?[] { -4, 1, null, 3 };
+            var cast = source.Cast<int?, int>();
+            Assert.Throws<NullReferenceException>(() => cast.ToList());
+        }
+
+        [Fact]
+        public void NullSource()
+        {
+            AssertExtensions.Throws<ArgumentNullException>("source", () => ((IReadOnlyList<object>)null).Cast<object, string>());
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var count = (new object[] { 0, 1, 2 }).Cast<object, int>().Count();
+            Assert.Equal(3, count);
+        }
+
+        #endregion
+    }
+
+}
---
+++ IReadOnlyListLinq/IReadOnlyListLinq.Cast.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IReadOnlyListLinq
+{
+    public static partial class IReadOnlyListLinq
+    {
+		public static IReadOnlyList<TResult> Cast<TSource, TResult>(this IReadOnlyList<TSource> source)
+		{
+            if (source is IReadOnlyList<TResult> result)
+                return result;
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            return new CastIterator<TSource, TResult>(source);
+		}
+
+        private sealed class CastIterator<TSource, TResult> : Iterator<TResult>
+        {
+            private readonly IReadOnlyList<TSource> _source;
+
+            public CastIterator(IReadOnlyList<TSource> source)
+            {
+                _source = source;
+            }
+
+            public sealed override TResult this[int index] => (TResult)(object)_source[index];
+
+            public sealed override int Count => _source.Count;
+
+            public sealed override Iterator<TResult> Clone() =>
+                new CastIterator<TSource, TResult>(_source);
+
+            private int _count = -1;
+            public sealed override bool MoveNext()
+            {
+                if (_count == -1)
+                    _count = Count;
+                if (++index >= _count)
+                {
+                    current = default;
+                    return false;
+                }
+                current = (TResult)(object)(_source[index]);
+                return true;
+            }
+        }
+
+        public static IReadOnlyList<TResult> Cast<TResult>(this IList source)
+        {
+            if (source is IReadOnlyList<TResult> result)
+                return result;
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            return new CastIterator<TResult>(source);
+        }
+
+        private sealed class CastIterator<TResult> : Iterator<TResult>
+        {
+            private readonly IList _source;
+
+            public CastIterator(IList source)
+            {
+                _source = source;
+            }
+
+            public sealed override TResult this[int index] => (TResult)_source[index];
+
+            public sealed override int Count => _source.Count;
+
+            public sealed override Iterator<TResult> Clone() =>
+                new CastIterator<TResult>(_source);
+
+            private int _count = -1;
+            public sealed override bool MoveNext()
+            {
+                if (_count == -1)
+                    _count = Count;
+                if (++index >= _count)
+                {
+                    current = default;
+                    return false;
+                }
+                current = (TResult)(_source[index]);
+                return true;
+            }
+        }
+    }
+}
