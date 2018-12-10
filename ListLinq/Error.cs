e2d203f9a8ade361a29204966dda09033d298c95
yair halberstadt <yairhalberstadt@hotmail.co.uk>
2018-12-10 08:52:39 +0000

    Update to use latest version of InvalidStateException

--- ListLinq/Error.cs
+++ ListLinq/Error.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using YLib;
 
 namespace ListLinq
 {
     internal static class Error
 	{
-		public static Exception InvalidState => new InvalidStateException.InvalidStateException(
+		public static Exception InvalidState => new InvalidStateException(
 			$"This Exception should never be thrown, and indicates that an invalid state has been reached. If this exception is thrown in your code, please contact the Library maintainers at {RepoInformation.IssueReportingLocation}. DO NOT catch this exception, as your program will not be in a predictable state.");
-
 	}
 }
